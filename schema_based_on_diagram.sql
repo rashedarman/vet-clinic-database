3bc08ed816f298518131b8820135e31bc5745b00
Rashed Arman <rashedarman21@gmail.com>
2023-03-07 22:32:54 +0600

    feat: create a schema based on diagram

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,35 @@
+CREATE TABLE patients (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(100),
+  date_of_birth DATE
+);
+
+CREATE TABLE medical_histories (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  admitted_at TIMESTAMP,
+  patient_id INT REFERENCES patients(id),
+  status VARCHAR(40)
+);
+
+CREATE TABLE treatments (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  type VARCHAR(40),
+  name VARCHAR(100)
+);
+
+CREATE TABLE invoice_items (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  unit_price NUMERIC(10, 2), -- DECIMAL is also valid
+  quantity INT,
+  total_price NUMERIC(10, 2),
+  invoice_id INT REFERENCES invoices(id),
+  treatment_id INT REFERENCES treatments(id)
+);
+
+CREATE TABLE invoices (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  total_amount NUMERIC(10, 2),
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history_id INT REFERENCES medical_histories(id)
+);
