c77910027bc296b567f74aa5649732c428c3f936
Rashed Arman <rashedarman21@gmail.com>
2023-03-07 21:35:15 +0600

    fix: remove unusual line terminator

--- queries.sql
+++ queries.sql
@@ -183,7 +183,7 @@ SELECT species.name as species, COUNT(*) AS count FROM visits
     WHERE vets.name = 'Maisy Smith'
     GROUP BY species.name ORDER BY count DESC LIMIT 1;
 
---- Performance queries 
-EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animals_id = 4; 
+--- Performance queries
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animals_id = 4;
 EXPLAIN ANALYZE SELECT * FROM visits where vets_id = 2;
- EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
